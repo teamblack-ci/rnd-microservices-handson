2bc5f2022b5896275f016ca6b6b3c9714ae6fac1
Oliver Trosien <otrosien@epages.com>
2015-08-31 15:45:32 +0200

    catalog initialization refactoring

--- catalog/src/main/java/com/epages/microservice/handson/catalog/PizzaInitializer.java
+++ catalog/src/main/java/com/epages/microservice/handson/catalog/PizzaInitializer.java
@@ -8,22 +8,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class PizzaInitializer  {
 
+    private final PizzaRepository pizzaRepository;
+
     @Autowired
     public PizzaInitializer(PizzaRepository pizzaRepository) {
-        Pizza pizza = new Pizza();
-        pizza.setName("Pizza Salami");
-        pizza.setDescription("The classic - Pizza Salami");
-        pizza.setToppings(Sets.newHashSet(CHEESE, SALAMI));
-        pizza.setImageUrl("http://www.sardegna-rustica.de/images/pizza.jpg");
+        this.pizzaRepository = pizzaRepository;
+    }
 
-        pizzaRepository.save(pizza);
+    public void init() {
+        pizzaRepository.save(salamiPizza());
+        pizzaRepository.save(hawaiiPizza());
+    }
 
+    private Pizza hawaiiPizza() {
+        Pizza pizza;
         pizza = new Pizza();
         pizza.setName("Pizza Hawaii");
         pizza.setDescription("The exotic among the classics - Pizza Hawaii");
         pizza.setToppings(Sets.newHashSet(CHEESE, HAM, PINEAPPLE));
         pizza.setImageUrl("http://www.heimfrost.de/_images/artikel/listenbilder/08834.jpg");
+        return pizza;
+    }
 
-        pizzaRepository.save(pizza);
+    private Pizza salamiPizza() {
+        Pizza pizza = new Pizza();
+        pizza.setName("Pizza Salami");
+        pizza.setDescription("The classic - Pizza Salami");
+        pizza.setToppings(Sets.newHashSet(CHEESE, SALAMI));
+        pizza.setImageUrl("http://www.sardegna-rustica.de/images/pizza.jpg");
+        return pizza;
     }
 }
