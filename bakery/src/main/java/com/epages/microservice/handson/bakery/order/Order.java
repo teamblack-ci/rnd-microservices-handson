ec22eafbbf71df335d31995d721fec32ce76e051
Jens Fischer <j.fischer@epages.com>
2015-09-04 14:01:45 +0200

    Jackson is powerful

--- bakery/src/main/java/com/epages/microservice/handson/bakery/order/Order.java
+++ bakery/src/main/java/com/epages/microservice/handson/bakery/order/Order.java
@@ -1,16 +1,22 @@
 package com.epages.microservice.handson.bakery.order;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import static com.google.common.base.MoreObjects.toStringHelper;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.google.common.base.MoreObjects.toStringHelper;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 public class Order {
 
-    @JsonIgnore
     private URI orderLink;
 
     private List<LineItem> orderItems = new ArrayList<>();
@@ -19,6 +25,8 @@ public class Order {
         return orderLink;
     }
 
+    @JsonProperty("_links")
+    @JsonDeserialize(using = OrderLinkDeserializer.class)
     public void setOrderLink(URI orderLink) {
         this.orderLink = orderLink;
     }
@@ -34,5 +42,16 @@ public class Order {
     @Override
     public String toString() {
         return toStringHelper(this).add("orderItems", orderItems).toString();
+    }
+
+    static class OrderLinkDeserializer extends JsonDeserializer<URI> {
+        @Override
+        public URI deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException {
+            final ObjectCodec codec = jsonParser.getCodec();
+            final JsonNode links = codec.readTree(jsonParser);
+            final JsonNode self = links.get("self");
+            final JsonNode href = self.get("href");
+            return URI.create(href.textValue());
+        }
     }
 }
