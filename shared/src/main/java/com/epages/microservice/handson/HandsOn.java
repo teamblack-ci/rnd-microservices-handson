883496480d8c8c5667c90dccb5a93699928b66ce
Oliver Trosien <otrosien@epages.com>
2015-09-07 11:38:03 +0200

    bump shared project version

--- shared/src/main/java/com/epages/microservice/handson/HandsOn.java
+++ shared/src/main/java/com/epages/microservice/handson/HandsOn.java
@@ -1,8 +1,5 @@
 package com.epages.microservice.handson;
 
-// Marker class for handson package
-public final class HandsOn {
-    private HandsOn() {
-        // don't instance me
-    }
+// Marker interface for handson package
+public @interface HandsOn {
 }
