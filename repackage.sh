b295a863f8399cea71a586cd7551ac522b117107
Jens Fischer <j.fischer@epages.com>
2015-09-07 13:34:07 +0200

    running refs in docker

--- repackage.sh
+++ repackage.sh
@@ -4,39 +4,39 @@ echo
 echo "#############################################"
 echo "# building 'shared'                         #"
 echo "#############################################"
-./shared/gradlew -p shared clean install
+./shared/gradlew -p shared install
 
 echo
 echo "#############################################"
 echo "# building 'catalog'                        #"
 echo "#############################################"
-./catalog/gradlew -p catalog clean check bootRepackage
+./catalog/gradlew -p catalog bootRepackage -x test
 docker-compose build --no-cache catalog
 
 echo
 echo "#############################################"
 echo "# building 'order'                          #"
 echo "#############################################"
-./order/gradlew -p order clean check bootRepackage
+./order/gradlew -p order bootRepackage -x test
 docker-compose build --no-cache order
 
 echo
 echo "#############################################"
 echo "# building 'bakery'                         #"
 echo "#############################################"
-./bakery/gradlew -p bakery clean check bootRepackage
+./bakery/gradlew -p bakery bootRepackage -x test
 docker-compose build --no-cache bakery
 
 echo
 echo "#############################################"
 echo "# building 'delivery'                       #"
 echo "#############################################"
-./delivery/gradlew -p delivery clean check bootRepackage
+./delivery/gradlew -p delivery bootRepackage -x test
 docker-compose build --no-cache delivery
 
 echo
 echo "#############################################"
 echo "# building 'order-ui'                       #"
 echo "#############################################"
-./order-ui/gradlew -p order-ui clean check bootRepackage
+./order-ui/gradlew -p order-ui bootRepackage -x test
 docker-compose build --no-cache orderui
